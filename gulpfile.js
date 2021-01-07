efbb8011f07454730d2ac01b0ffacd1fb6c59e5f
ledeDEV02 <vladimirlebedev00010@gmail.com>
2021-01-07 15:43:55 +0500

    added gulp sync

--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,27 @@
-const { src, dest, watch } = require('gulp');
+const { src, dest, watch, parallel } = require('gulp');
 const scss = require('gulp-sass');
 const concat = require('gulp-concat');
+const browserSync = require('browser-sync').create();
+
+function browsersync() {
+	browserSync.init({
+		server: {
+			baseDir: "app/"
+		}
+	});
+}
 function styles() {
 	return src('app/**/*.scss')
 		.pipe(scss({ outputStyle: 'compressed' }))
 		.pipe(concat('style.min.css'))
 		.pipe(dest('app/css'))
+		.pipe(browserSync.stream())
 }
 function watching() {
 	watch(['app/scss/**/*.scss'], styles)
+	watch(['app/*.html']).on('change', browserSync.reload)
 }
 exports.styles = styles;
 exports.watching = watching;
+exports.browsersync = browsersync;
+exports.default = parallel(browsersync, watching);
