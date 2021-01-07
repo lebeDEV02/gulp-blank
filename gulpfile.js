6ce1a130530643881eac27814dfa99198400590e
ledeDEV02 <vladimirlebedev00010@gmail.com>
2021-01-07 15:11:47 +0500

    added concatenation

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,10 @@
 const { src, dest } = require('gulp');
 const scss = require('gulp-sass');
+const concat = require('gulp-concat');
 function styles() {
 	return src('app/**/*.scss')
 		.pipe(scss({ outputStyle: 'compressed' }))
+		.pipe(concat('style.min.css'))
 		.pipe(dest('app/css'))
 }
 exports.styles = styles;
