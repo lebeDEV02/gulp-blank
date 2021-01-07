296167f57392791611709698e2c1197d2fe3992f
ledeDEV02 <vladimirlebedev00010@gmail.com>
2021-01-07 15:16:14 +0500

    added watching

--- gulpfile.js
+++ gulpfile.js
@@ -1,4 +1,4 @@
-const { src, dest } = require('gulp');
+const { src, dest, watch } = require('gulp');
 const scss = require('gulp-sass');
 const concat = require('gulp-concat');
 function styles() {
@@ -7,4 +7,8 @@ function styles() {
 		.pipe(concat('style.min.css'))
 		.pipe(dest('app/css'))
 }
+function watching() {
+	watch(['app/scss/**/*.scss'], styles)
+}
 exports.styles = styles;
+exports.watching = watching;
